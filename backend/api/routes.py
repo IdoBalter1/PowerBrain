a63f5d88acab46cc58f38498126371a847b4121b
Ido Balter <idobalter@gmail.com>
2026-01-22 12:15:56 +0000

    add login register jwt routes

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from backend.models.user import User
-from api.auth import create_access_token
+from api.auth import create_access_token, get_current_user, decode_JWT
 from pydantic import BaseModel
 from backend.core.models import users_db
+
 
 user_router = APIRouter(prefix = "/auth", tags=["auth"]) # continaer/grouping mechanism that groups realted endpoing together like a sub-app that gets merged into main app
 class LoginRequest(BaseModel):
@@ -14,14 +15,16 @@ class LoginRequest(BaseModel):
 async def register(user: User):
     if user.username in users_db:
         raise HTTPException(
-            status_code = 400,
+            status_code = 409,
             detail="Username already exists"
         )
     users_db[user.username] = user
 
+    access_token = create_access_token(data={"sub": user.username})
     return {
-        "message": "User registered successfully",
-        "username": user.username
+        "access_token": access_token,
+        "token_type": "bearer",
+        "message": "User registered successfully"
     }
 
 @user_router.post("/login")
@@ -31,7 +34,7 @@ async def login(user: LoginRequest):
             status_code = 401,
             detail="Incorrect username or password" 
         )
-    elif user.username in users_db and users_db[user.username].password != user.password:
+    if users_db[user.username].password != user.password:
         raise HTTPException(
             status_code = 401,
             detail="Incorrect username or password"
